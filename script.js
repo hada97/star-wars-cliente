786b9fc40d747108859fb6ece21d9ebe0c8f786e
Adailson <adailson65@gmail.com>
2024-10-18 18:35:53 -0300

    add busca por id

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+
 document.getElementById('fetch-button').addEventListener('click', async () => {
     const endpoint = document.getElementById('endpoint').value;
-    const response = await fetch(`https://swapi.dev/api/${endpoint}/`);
-    const data = await response.json();
-    document.getElementById('result').textContent = JSON.stringify(data, null, 2);
+    const id = document.getElementById('id-input').value;
+    const url = id ? `https://swapi.dev/api/${endpoint}/${id}/` : `https://swapi.dev/api/${endpoint}/`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error('Erro na requisição: ' + response.statusText);
+        const data = await response.json();
+        document.getElementById('result').textContent = JSON.stringify(data, null, 2);
+    } catch (error) {
+        document.getElementById('result').textContent = 'Erro: ' + error.message;
+    }
+    
+    
 });
+
+
